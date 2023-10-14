32fdb9cc16cbac6115443a00072e5b9527cbbe30
= <gui.silva93@hotmail.com>
2023-10-14 17:33:49 -0300

    corrção do botão de voltar.

--- src/js/success.js
+++ src/js/success.js
@@ -7,3 +7,9 @@ const resultText = document.getElementById('confirmation')
 console.log(resultText);
 
 resultText.innerHTML = 'A confirmation email has been sent to <span>' + decodeURIComponent(emailReceived) + '</span>. Please open it and click the button inside to confirm your subscrition.'
+
+const btnBack = document.getElementById('btnBack')
+
+btnBack.addEventListener('click', () =>{
+    window.history.back()
+})
